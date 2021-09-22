26f288a16e14257c7e1736c837b54731d695f978
Thomas Göttgens <tgoettgens@gmail.com>
2021-09-22 21:27:38 +0200

    Fix textarea escaping
    Address is a textarea field and needs to preserve linebreaks and whitespace

--- classes/widgets/class-astra-widget-address.php
+++ classes/widgets/class-astra-widget-address.php
@@ -225,6 +225,9 @@ if ( ! class_exists( 'Astra_Widget_Address' ) ) :
 			$instance['display-icons'] = isset( $new_instance['display-icons'] ) ? (bool) $new_instance['display-icons'] : false;
 
 			$instance = array_map( 'sanitize_text_field', $instance );
+			
+			// Address is a textarea field and needs to preserve linebreaks and whitespace
+                        $instance['address'] = sanitize_textarea_field($new_instance['address']);
 
 			return $instance;
 		}
